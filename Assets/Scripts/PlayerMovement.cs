9ce45491a7a5f392f2f321a3737ee17bd1da39d9
Andres Rodriguez <andresrodriguezesteban@gmail.com>
2023-09-05 18:39:58 -0300

    added walls, improved movement code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,37 +8,38 @@ public class PlayerMovement : MonoBehaviour
     public float topSpeed;
     public float stopRate;
     Rigidbody2D rb2D;
-    bool movingX;
-    bool movingY;
+    float movementX;
+    float movementY;
 
-    void Awake()
-    {
+    void Awake () {
         rb2D = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
-    {
-        Movementhandler();
-        StopHandler();
+    private void Update () {
+        movementX = Input.GetAxisRaw("Horizontal");
+        movementY = Input.GetAxisRaw("Vertical");
     }
 
-    private void Movementhandler () {
-        if (Input.GetAxisRaw("Horizontal") != 0) { rb2D.AddForce(new Vector2(speed * Input.GetAxisRaw("Horizontal"), 0)); }
-
-        if (Input.GetAxisRaw("Vertical") != 0) { rb2D.AddForce(new Vector2(0, speed * Input.GetAxisRaw("Vertical"))); }
-
-        movingX = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D);
-        movingY = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S);
+    void FixedUpdate () {
+        MovementHandler();
+        VelocityHandler();
     }
 
-    private void StopHandler () {
-        if (!movingX) rb2D.velocity = new Vector2(rb2D.velocity.x / stopRate, rb2D.velocity.y);
-        if (!movingY) rb2D.velocity = new Vector2(rb2D.velocity.x, rb2D.velocity.y / stopRate);
+    private void MovementHandler () {
+        if (movementX != 0) { rb2D.AddForce(new Vector2(speed * movementX, 0)); }
+        if (movementY != 0) { rb2D.AddForce(new Vector2(0, speed * movementY)); }
+    }
+
+    private void VelocityHandler () {
+        if (movementX == 0) rb2D.velocity = new Vector2(rb2D.velocity.x / stopRate, rb2D.velocity.y);
+        if (movementY == 0) rb2D.velocity = new Vector2(rb2D.velocity.x, rb2D.velocity.y / stopRate);
 
         if (rb2D.velocity.x > topSpeed) rb2D.velocity = new Vector2(topSpeed, rb2D.velocity.y);
         if (rb2D.velocity.y > topSpeed) rb2D.velocity = new Vector2(rb2D.velocity.x, topSpeed);
 
         if (rb2D.velocity.x < (topSpeed * -1)) rb2D.velocity = new Vector2((topSpeed * -1), rb2D.velocity.y);
         if (rb2D.velocity.y < (topSpeed * -1)) rb2D.velocity = new Vector2(rb2D.velocity.x, (topSpeed * -1));
+
+        if (rb2D.angularVelocity > topSpeed) rb2D.angularVelocity = topSpeed;
     }
 }
