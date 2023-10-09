9e4b01448abaa6779983da197605352eda553b90
CoffeAddict <andresrodriguezesteban@gmail.com>
2023-10-09 06:26:31 -0300

    finished waypoints system

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-    // public float movementSpeed;
+    public float movementSpeed;
     public float rotationSpeed;
-    // public float topSpeed;
-    // public float stopRate;
+    public float rotationOffset;
+    public float topSpeed;
+    public float stopRate;
     public GameObject pathParent = null;
+    public float waypointSwitchRange = 4;
+    public bool followWaypoint = false;
     private Transform[] pathList;
+
     private bool hasPath;
     private Transform currentWaypoint = null;
-    private int currentWaypointIndex = -1;
-    private bool followWaypoint = false;
+    private int currentWaypointIndex = 0;
 
     Rigidbody2D rb2D;
 
@@ -38,31 +41,56 @@ public class EnemyMovement : MonoBehaviour
     }
 
     void FixedUpdate () {
-        if (followWaypoint == true) followCurrentWaypoint();
+        if (followWaypoint == true) {
+            checkWaypointDistance(currentWaypoint);
+            rotateToWaypoint();
+            goForward();
+        };
+
+    }
+
+    void checkWaypointDistance (Transform waypoint) {
+        float distance = Vector3.Distance (waypoint.position, transform.position);
+
+        if (distance <= waypointSwitchRange) updateWaypoint();
     }
 
     void updateWaypoint () {
-        currentWaypoint = currentWaypoint == null ? pathList[0] : pathList[currentWaypointIndex++];
+        rb2D.velocity = new Vector3(rb2D.velocity.x / stopRate , rb2D.velocity.y / stopRate);
+        rb2D.angularVelocity = rb2D.angularVelocity / stopRate ;
+
+        if (currentWaypointIndex >= pathList.Length) currentWaypointIndex = 0;
+        currentWaypoint = currentWaypoint == null ? pathList[0] : pathList[currentWaypointIndex];
+
+        currentWaypointIndex++;
+
         followWaypoint = true;
     }
 
-    void followCurrentWaypoint () {
-        Vector3 targetRotation = currentWaypoint.position - transform.position;
+    void rotateToWaypoint () {
+        Vector3 targetPosition = currentWaypoint.position - transform.position;
 
-        float rotationModifier = 90;
+        // I had to do some research to understand this
+        // Mathf.Atan2 returns the angle in radians whose tangent is y/x (the angle were we want to look at)
+        // Mathf.Rad2Deg transforms radians to degrees
+        float angle = Mathf.Atan2(targetPosition.y, targetPosition.x) * Mathf.Rad2Deg - rotationOffset;
 
-        float angle = Mathf.Atan2(targetRotation.y, targetRotation.x) * Mathf.Rad2Deg - rotationModifier;
+        // Returns a rotation on the Z angle (Vector3.forward)
+        Quaternion desiredRotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
-        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
-
-        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * rotationSpeed);
+        // Interpolates a rotation from current rotation to desiredRotation on an specified time
+        transform.rotation = Quaternion.Slerp(transform.rotation, desiredRotation, Time.deltaTime * rotationSpeed);
     }
 
-    // Update is called once per frame
-    // void Update()
-    // {
+    void goForward () {
+        rb2D.AddRelativeForce(new Vector2(0, movementSpeed));
 
-    // }
+        if (rb2D.velocity.x > topSpeed) rb2D.velocity = new Vector2(topSpeed, rb2D.velocity.y);
+        if (rb2D.velocity.y > topSpeed) rb2D.velocity = new Vector2(rb2D.velocity.x, topSpeed);
+
+        if (rb2D.velocity.x < (topSpeed * -1)) rb2D.velocity = new Vector2((topSpeed * -1), rb2D.velocity.y);
+        if (rb2D.velocity.y < (topSpeed * -1)) rb2D.velocity = new Vector2(rb2D.velocity.x, (topSpeed * -1));
+    }
 }
 
 
